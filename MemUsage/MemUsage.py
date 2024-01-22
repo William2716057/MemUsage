4507538b8069696e701745ee76589bb333f586e4
User <Willspri@gmail.com>
2024-01-22 15:11:57 +1300

    added

--- MemUsage/MemUsage.py
+++ MemUsage/MemUsage.py
@@ -5,3 +5,9 @@ while True:
     print(psutil.cpu_percent())
     print(psutil.virtual_memory().percent)
     time.sleep(1)
+    
+#visualise values
+def displayUsage(cpu_usage, mem_usage, bars=50):
+    cpu_percent = (cpu_usage / 100.0)
+    cpu_bar '' * int(cpu_percent * bars) + ' ' * (bars - int(cpu_percent * bars))
+    mem_percent = (mem_usage / 100.0)
